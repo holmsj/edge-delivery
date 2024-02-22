406c2ea510d89d69a8742ede0890f40640f1b771
Jeff Holmquist <jholmqui@adobe.com>
2024-02-22 14:55:16 -0700

    Add stocks block

---
+++ blocks/stocks/stocks.js
@@ -0,0 +1,25 @@
+async function fetchStockPrice(id) {
+    const response = await fetch('https://65d7b32527d9a3bc1d7ba74e.mockapi.io/price?id=' + id).then((response) => response.json())
+    return response;
+  }
+
+export default function decorate(block) {
+    /* change to ul, li */
+    const ul = document.createElement('ul');
+    [...block.children].forEach((row) => {
+      const li = document.createElement('li');
+      while (row.firstElementChild) li.append(row.firstElementChild);
+      [...li.children].forEach((div) => {
+        div.className = 'stock-id';
+        fetchStockPrice(div.textContent).then(data => {
+            const priceDiv = document.createElement("div");
+            priceDiv.className = "stock-price";
+            priceDiv.append(data[0].price);
+            li.append(priceDiv);
+          });;
+      });
+      ul.append(li);
+    });
+    block.textContent = '';
+    block.append(ul);
+  }
