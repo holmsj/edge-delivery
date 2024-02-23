eaed22e9942d9d2cda237e89325f92886e4f604a
Jeff Holmquist <jholmqui@adobe.com>
2024-02-23 09:30:06 -0700

    Enhance stocks block

--- blocks/stocks/stocks.js
+++ blocks/stocks/stocks.js
@@ -1,25 +1,26 @@
 async function fetchStockPrice(id) {
-    const response = await fetch('https://65d7b32527d9a3bc1d7ba74e.mockapi.io/price?id=' + id).then((response) => response.json())
-    return response;
+  const response = await fetch(`https://65d7b32527d9a3bc1d7ba74e.mockapi.io/price?id=${id}`).then((response) => response.json())
+  return response;
   }
 
 export default function decorate(block) {
-    /* change to ul, li */
-    const ul = document.createElement('ul');
-    [...block.children].forEach((row) => {
-      const li = document.createElement('li');
-      while (row.firstElementChild) li.append(row.firstElementChild);
-      [...li.children].forEach((div) => {
-        div.className = 'stock-id';
-        fetchStockPrice(div.textContent).then(data => {
-            const priceDiv = document.createElement("div");
-            priceDiv.className = "stock-price";
-            priceDiv.append(data[0].price);
-            li.append(priceDiv);
-          });;
-      });
-      ul.append(li);
+  /* change to ul, li */
+  const ul = document.createElement('ul');
+  [...block.children].forEach((row) => {
+    const li = document.createElement('li');
+    while (row.firstElementChild) li.append(row.firstElementChild);
+    [...li.children].forEach((div) => {
+    div.className = 'stock-id';
+    fetchStockPrice(div.textContent).then(data => {
+      const priceDiv = document.createElement("div");
+      priceDiv.className = "stock-price";
+      if (data[0].dailyGain) li.classList.add('daily-gain')
+      priceDiv.append(data[0].price);
+      li.append(priceDiv);
+      });;
     });
-    block.textContent = '';
-    block.append(ul);
+    ul.append(li);
+  });
+  block.textContent = '';
+  block.append(ul);
   }
